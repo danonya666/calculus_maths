4ea1782138436eb8281e418afe9bccae8eabd923
Daniil <32582330+danonya666@users.noreply.github.com>
2019-04-23 19:32:25 +0300

    finished + pointed

---
+++ calc_maths5/2.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+import math
+
+
+def rk4(f, x0, y0, x1, n):
+    vx = [0] * (n + 1)
+    vy = [0] * (n + 1)
+    h = (x1 - x0) / float(n)
+    vx[0] = x = x0
+    vy[0] = y = y0
+    for i in range(1, n + 1):
+        k1 = h * f(x, y)
+        k2 = h * f(x + 0.5 * h, y + 0.5 * k1)
+        k3 = h * f(x + 0.5 * h, y + 0.5 * k2)
+        k4 = h * f(x + h, y + k3)
+        vx[i] = x = x0 + i * h
+        vy[i] = y = y + (k1 + k2 + k2 + k3 + k3 + k4) / 6
+    return vx, vy
+
+
+f = lambda x, y: (y**2 * math.log(x) - y)/x
+
+
+vx, vy = rk4(f, 1, 1, 2.6, 100)
+for x, y in list(zip(vx, vy))[::10]:
+    print("%4.1f %10.5f" % (x, y))
